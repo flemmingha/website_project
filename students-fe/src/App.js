a4a03c834a05315ff0f0c49ceeb79711792ecfff
Flemming <flemming_ha@hotmail.com>
2023-10-20 11:55:53 +0200

    Managed to fetch the AAPL ticker from a stock API

--- students-fe/src/App.js
+++ students-fe/src/App.js
@@ -1,8 +1,7 @@
 import React, { Component, Fragment } from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Test from "./components/Test";
-import StockAPI from "./components/StockAPI";
+import Stock from "./components/StockAPI";
 import PersonList from "./components/Test";
 
 class App extends Component {
@@ -12,7 +11,7 @@ class App extends Component {
         <Header />
         <Home />
         <PersonList />
-         {/* <Stock /> */}
+          <Stock />
       </Fragment>
     );
   }
--- students-fe/src/components/StockAPI.js
+++ students-fe/src/components/StockAPI.js
@@ -1,67 +1,7 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
-class Stock extends Component {
-  state = {
-    persons: []
-  }
-
-  componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
-      .then(res => {
-        const persons = res.data;
-        this.setState({ persons });
-      })
-  }
-
-  render() {
-    return (
-      <ul>
-        {
-          this.state.persons
-            .map(person =>
-              <li key={person.id}>{person.name}</li>
-            )
-        }
-      </ul>
-    )
-  }
-}
-
-export default Stock;
-
-
-//import React from 'react';
-//import axios from 'axios';
-
-/*
-import React, { Component } from "react";
-
-class Stock extends Component {
-  render() {
-    return (
-      <div className="text-center">
-        <img
-          src="C:\Users\flemm\Desktop\New Website\web_env\django_react_proj\students-fe\public\IMG_2400.JPG"
-          width="300"
-          className="img-thumbnail"
-          style={{ marginTop: "20px" }}
-        />
-        <hr />
-        <h5>
-          <i>Workation</i>
-        </h5>
-        <h1>Kombiner arbejde og ferie i de smukkeste omgivelser</h1>
-      </div>
-    );
-  };
-}
-
-export default Stock;
-
-*/
-
-  /*
- 
+class Stock extends Component { 
   state = {
     stockprices: []
   }
@@ -78,14 +18,10 @@ export default Stock;
   render() {
     return (
       <ul>
-        {
-          this.state.stockprices
-            .map(stockprice =>
-              <li key={stockprice.ticker}></li>
-            )
-        }
+        <li>{this.state.stockprices.ticker}</li>     
       </ul>
     )
   }
 }
-*/
+
+export default Stock;
